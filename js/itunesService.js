755d8a55ab7f5fb510217acec0bc9f61ec6e4c21
Daniel Gardner <bowshanks@gmail.com>
2016-09-01 20:38:09 -0600

    Finished

--- js/itunesService.js
+++ js/itunesService.js
@@ -4,9 +4,45 @@ angular.module('itunes').service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
+  this.urlBuilder = function(term, media, limit, explicit) {
+    return 'https://itunes.apple.com/search?term=' + term + '&media=' + media + '&limit=' + limit + '&explicit=' + explicit + '&callback=JSON_CALLBACK'
+  }
 
     //Code here
-    
+    this.getArtistInfo = function(term, mediatype, limit, explicit){
+      var url = this.urlBuilder(term,mediatype,limit,explicit);
+
+      var deferred = $q.defer();
+      $http({
+        method: 'JSONP',
+        url: url
+      }).then(function(response){
+        var songData = response.data.results;
+        var formattedSongData = [];
+        for (var song in songData){
+          formattedSongData.push(
+            {
+              AlbumArt: songData[song]['artworkUrl100'],
+              Artist: songData[song]['artistName'],
+              Song: songData[song]['trackName'],
+              Collection: songData[song]['collectionName'],
+              CollectionPrice: songData[song]['collectionPrice'],
+              Play: songData[song]['previewUrl'],
+              Genre: songData[song]['primaryGenreName'],
+              trackLength: songData[song]['trackTimeMillis'] ? Math.floor(songData[song]['trackTimeMillis']/1000/60) + ':' + ('00' + Math.ceil((songData[song]['trackTimeMillis']/1000) % 60)).slice(-2) : '--:--'
+            }
+          )
+        }
+
+        deferred.resolve(formattedSongData)
+      })
+
+      return deferred.promise;
+    }
+
+
+
+
 });
